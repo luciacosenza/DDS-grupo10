61d260aa3c44889274634eb88584f73026bad7c3
Sebabalas <sbals@frba.utn.edu.ar>
2024-09-06 19:59:46 -0300

    Mejoro navegabilidad: se ven correctamente los links para cada tipo de usuario, se ve marcado el link con azul en la pagina actual

--- proyecto_heladeras_solidarias/src/main/resources/static/js/validar_usuario.js
+++ proyecto_heladeras_solidarias/src/main/resources/static/js/validar_usuario.js
@@ -50,104 +50,64 @@ if(document.querySelector("#iniciar-sesion")){
     }
 
 let navbar = document.querySelector(".navbar");
-
+let nombrePaginaActual;
 if(document.querySelector("#index")){
+    nombrePaginaActual = "index"
     const tipo_usuario_guardado = sessionStorage.getItem('usuario');
     if(tipo_usuario_guardado == 'admin'){
-        renderAdmin()
+        renderAdmin(nombrePaginaActual)
     }
     if(tipo_usuario_guardado == 'colHumano'){
-        renderColHumano()
+        renderColHumano(nombrePaginaActual)
     }
     if(tipo_usuario_guardado == 'colJuridico'){
-        renderColJuridico()
+        renderColJuridico(nombrePaginaActual)
     }     
 }
 
-function renderAdmin(){
-    navbar.innerHTML = ` <div class="container-fluid">
-                                <a class="navbar-brand" href="#">
-                                    <img src="./assets/logo_negro.png" alt="Logo" height="50">
-                                </a>
-                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon"></span>
-                                </button>
-                                <div class="collapse navbar-collapse" id="navbarNav">
-                                    <ul class="navbar-nav ms-auto">
-                                        <li class="nav-item">
-                                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/migracion_datos.html">Migrar archivo</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/alertas.html">Alertas</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">Reportes</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/quienes_somos.html">Quiénes somos</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/como_participar.html">Cómo participar</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/contacto.html">Contacto</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/colaborar.html">Colaborar</a>
-                                        </li>
-                                    </ul>
-                                <div class="d-flex flex-column ms-lg-3">
-                                    <span class="icon-perfil material-symbols-outlined">
-                                        account_circle
-                                    </span>
-                                    admin
-                                </div>
-                            </div>
-                        </div>`
+if(document.querySelector("#quienes-somos")){
+    console.log("Entre a quienes somos");
+    nombrePaginaActual = "quienes_somos"
+    const tipo_usuario_guardado = sessionStorage.getItem('usuario');
+    if(tipo_usuario_guardado == 'admin'){
+        renderAdmin(nombrePaginaActual)
+    }
+    if(tipo_usuario_guardado == 'colHumano'){
+        renderColHumano(nombrePaginaActual)
+    }
+    if(tipo_usuario_guardado == 'colJuridico'){
+        renderColJuridico(nombrePaginaActual)
+    }     
 }
 
-function renderColHumano(){
-    navbar.innerHTML = ` <div class="container-fluid">
-                                <a class="navbar-brand" href="#">
-                                    <img src="./assets/logo_negro.png" alt="Logo" height="50">
-                                </a>
-                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon"></span>
-                                </button>
-                                <div class="collapse navbar-collapse" id="navbarNav">
-                                    <ul class="navbar-nav ms-auto">
-                                        <li class="nav-item">
-                                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/quienes_somos.html">Quiénes somos</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/como_participar.html">Cómo participar</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/contacto.html">Contacto</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/colaborar_personas_fisicas.html">Colaborar</a>
-                                        </li>
-                                    </ul>
-                                <div class="d-flex flex-column align-items-center ms-lg-3">
-                                    <span class="icon-perfil material-symbols-outlined">
-                                        account_circle
-                                    </span>
-                                </div>
-                            </div>
-                        </div>`
+if(document.querySelector("#como-participar")){
+    console.log("Entre a como participar");
+    nombrePaginaActual = "como_participar"
+    const tipo_usuario_guardado = sessionStorage.getItem('usuario');
+    if(tipo_usuario_guardado == 'admin'){
+        renderAdmin(nombrePaginaActual)
+    }
+    if(tipo_usuario_guardado == 'colHumano'){
+        renderColHumano(nombrePaginaActual)
+    }
+    if(tipo_usuario_guardado == 'colJuridico'){
+        renderColJuridico(nombrePaginaActual)
+    }     
 }
 
-function renderColJuridico(){
+function renderAdmin(nombrePaginaActual) {
+    console.log("Renderizando navbar para Admin");
+    
+    console.log("nombrePaginaActual:", nombrePaginaActual);
+    let esIndex = nombrePaginaActual.includes("index");
+    console.log("esIndex:", esIndex);
+
+    const basePath = esIndex ? "./" : "../";
+    console.log("Base path para Admin:", basePath);
+
     navbar.innerHTML = ` <div class="container-fluid">
                                 <a class="navbar-brand" href="#">
-                                    <img src="./assets/logo_negro.png" alt="Logo" height="50">
+                                    <img src="${basePath}assets/logo_negro.png" alt="Logo" height="50">
                                 </a>
                                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                     <span class="navbar-toggler-icon"></span>
@@ -155,28 +115,136 @@ function renderColJuridico(){
                                 <div class="collapse navbar-collapse" id="navbarNav">
                                     <ul class="navbar-nav ms-auto">
                                         <li class="nav-item">
-                                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                                            <a class="nav-link" href="${basePath}index.html">Inicio</a>
                                         </li>
                                         <li class="nav-item">
-                                            <a class="nav-link" href="./pages/quienes_somos.html">Quiénes somos</a>
+                                            <a class="nav-link" href="${basePath}pages/migracion_datos.html">Migrar archivo</a>
                                         </li>
                                         <li class="nav-item">
-                                            <a class="nav-link" href="./pages/como_participar.html">Cómo participar</a>
+                                            <a class="nav-link" href="${basePath}pages/alertas.html">Alertas</a>
                                         </li>
                                         <li class="nav-item">
-                                            <a class="nav-link" href="./pages/contacto.html">Contacto</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="./pages/colaborar_personas_juridicas.html">Colaborar</a>
+                                            <a class="nav-link" href="${basePath}pages/reportes.html">Reportes</a>
                                         </li>
                                     </ul>
-                                <div class="d-flex flex-column align-items-center ms-lg-3">
-                                    <span class="icon-perfil material-symbols-outlined">
-                                        account_circle
-                                    </span>
+                                    <div class="d-flex flex-column ms-lg-3">
+                                        <span class="icon-perfil material-symbols-outlined">
+                                            account_circle
+                                        </span>
+                                        admin
+                                    </div>
                                 </div>
-                            </div>
-                        </div>`
+                            </div>`;
+
+    const linksNavegacion = document.querySelectorAll('.nav-link');
+    linkActivo(linksNavegacion, nombrePaginaActual);
+}
+
+function renderColHumano(nombrePaginaActual) {
+    console.log("Renderizando navbar para Colaborador Humano");
+    
+    console.log("nombrePaginaActual:", nombrePaginaActual);
+    let esIndex = nombrePaginaActual.includes("index");
+    console.log("esIndex:", esIndex);
+
+    const basePath = esIndex ? "./" : "../";
+    console.log("Base path para Admin:", basePath);
+
+    navbar.innerHTML = ` <div class="container-fluid">
+                                <a class="navbar-brand" href="#">
+                                    <img src="${basePath}assets/logo_negro.png" alt="Logo" height="50">
+                                </a>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarNav">
+                                    <ul class="navbar-nav ms-auto">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="${basePath}index.html">Inicio</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="${basePath}pages/quienes_somos.html">Quiénes somos</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="${basePath}pages/como_participar.html">Cómo participar</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="${basePath}pages/colaborar_personas_fisicas.html">Colaborar</a>
+                                        </li>
+                                    </ul>
+                                    <div class="d-flex flex-column align-items-center ms-lg-3">
+                                        <span class="icon-perfil material-symbols-outlined">
+                                            account_circle
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>`;
+    const linksNavegacion = document.querySelectorAll('.nav-link');
+    linkActivo(linksNavegacion, nombrePaginaActual);
+}
+
+function renderColJuridico(nombrePaginaActual) {
+    console.log("Renderizando navbar para Colaborador Juridico");
+    
+    console.log("nombrePaginaActual:", nombrePaginaActual);
+    let esIndex = nombrePaginaActual.includes("index");
+    console.log("esIndex:", esIndex);
+
+    const basePath = esIndex ? "./" : "../";
+    console.log("Base path para Admin:", basePath);
+
+    navbar.innerHTML = ` <div class="container-fluid">
+                                <a class="navbar-brand" href="#">
+                                    <img src="${basePath}assets/logo_negro.png" alt="Logo" height="50">
+                                </a>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarNav">
+                                    <ul class="navbar-nav ms-auto">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="${basePath}index.html">Inicio</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="${basePath}pages/quienes_somos.html">Quiénes somos</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="${basePath}pages/como_participar.html">Cómo participar</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="${basePath}pages/colaborar_personas_juridicas.html">Colaborar</a>
+                                        </li>
+                                    </ul>
+                                    <div class="d-flex flex-column align-items-center ms-lg-3">
+                                        <span class="icon-perfil material-symbols-outlined">
+                                            account_circle
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>`;
+                            
+    const linksNavegacion = document.querySelectorAll('.nav-link');
+    linkActivo(linksNavegacion, nombrePaginaActual);
+}
+
+function linkActivo(linksNavegacion, nombrePaginaActual) {
+    console.log("Activando el link correspondiente a la página:", nombrePaginaActual);
+    
+    linksNavegacion.forEach(link => {
+        const href = link.getAttribute('href');
+        console.log("Verificando link:", href);
+
+        if (href.includes(nombrePaginaActual)) {
+            console.log(`Marcando como activo el link: ${href}`);
+            link.classList.add('active');
+            link.setAttribute('aria-current', 'page');
+            link.setAttribute('href', '#'); // Cambiar href a '#' para la página activa
+        } else {
+            link.classList.remove('active');
+            link.removeAttribute('aria-current');
+            link.setAttribute('href', href); // Mantener el href original en las demás páginas
+        }
+    });
 }
 
 // Validación personalizada de Bootstrap
